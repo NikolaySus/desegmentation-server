480dcc88a441af17c8e8915fce680b6628ea8364
NikolaySus <gorkunovnm@gmail.com>
2024-05-11 23:49:00 +0300

    Сообщение прокидывается

--- consumer/main.go
+++ consumer/main.go
@@ -1,46 +1,117 @@
 package main
 
 import (
+    "bytes"
     "context"
+    "encoding/json"
     "fmt"
     "log"
+    "net/http"
     "os"
-    //"strings"
+    "os/signal"
+    "syscall"
     "time"
     kafka "github.com/segmentio/kafka-go"
 )
 
+type Msg struct {
+    Id          *string `json:"id"`
+    Username    *string `json:"username"`
+    Time        *string `json:"time"`
+    Text        *string `json:"text"`
+    Error       *string `json:"error"`
+}
+
+type ConsumerService struct {
+    conn            *kafka.Conn
+    applicationURL  string
+}
+
+func New(kafkaURL string, topic string, partition int, applicationURL string) (*ConsumerService, error) {
+    var err error
+    s := ConsumerService{}
+    s.conn, err = kafka.DialLeader(context.Background(), "tcp", kafkaURL, topic, partition)
+    if err != nil {
+        return nil, err
+    }
+    s.applicationURL = applicationURL
+    return &s, nil
+}
+
+func (s *ConsumerService) Receive(message Msg) error {
+    payload, err := json.Marshal(message)
+    if err != nil {
+        return fmt.Errorf(`failed to marshal message: {%s}`, err)
+    }
+    log.Print("sending json: ", string(payload))
+    resp, err := http.Post(s.applicationURL + "/receive", "application/json", bytes.NewReader(payload))
+    if err != nil {
+        return fmt.Errorf(`application service unavailable: {%s}`, err)
+    }
+    if resp.StatusCode >= 300 {
+        return fmt.Errorf(`application service failed: {%s}`, resp.Status)
+    }
+    return nil
+}
+
+func (s *ConsumerService) doJob() {
+    message, err := s.conn.ReadMessage(1e3)
+    if err != nil {
+        return
+    }
+    var msg Msg
+    if err := json.Unmarshal(message.Value, &msg); err != nil {
+        log.Printf(`failed to unmarshal message: {%s}`, err)
+        return
+    }
+    if msg.Id == nil || msg.Username == nil || msg.Time == nil || msg.Text == nil || msg.Error == nil {
+        log.Printf(`missing required field in message: {%s}`, msg)
+        return
+    }
+    if err := s.Receive(msg); err != nil {
+        log.Printf(`failed to send message: {%s}`, err)
+        return
+    }
+}
+
 func main() {
     kafkaURL := os.Getenv("kafkaURL")
     topic := os.Getenv("topic")
+    port := os.Getenv("port")
+    applicationURL := "http://" + os.Getenv("applicationURL")
     for i := 3; i > 0; i-- {
         log.Print("consumer sleeping for ", i, " seconds")
         time.Sleep(1 * time.Second)
     }
-    log.Print("consumer started with: ", kafkaURL, topic)
+    log.Print("consumer starting at :", port, " with kafkaURL=", kafkaURL, ", topic=", topic, " and applicationURL=", applicationURL)
     partition := 0
-    conn, err := kafka.DialLeader(context.Background(), "tcp", kafkaURL, topic, partition)
+    s, err := New(kafkaURL, topic, partition, applicationURL)
     if err != nil {
         log.Fatal("failed to dial leader:", err)
+        return
     }
-
-    conn.SetReadDeadline(time.Now().Add(10*time.Second))
-    batch := conn.ReadBatch(10e3, 1e6) // fetch 10KB min, 1MB max
-
-    b := make([]byte, 10e3) // 10KB max per message
-    for {
-        n, err := batch.Read(b)
-        if err != nil {
-            break
+    quit := make(chan os.Signal, 1)
+    signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+    go func() {
+        for {
+            select {
+            case <-quit:
+                return
+            default:
+                s.doJob()
+            }
         }
-        fmt.Println(string(b[:n]))
+    }()
+    <-quit
+    log.Println("shutdown consumer service ...")
+    if err := s.conn.Close(); err != nil {
+        log.Fatal("failed to close reader:", err)
     }
-
-    if err := batch.Close(); err != nil {
-        log.Fatal("failed to close batch:", err)
+    ctx, cancel := context.WithTimeout(context.Background(), 1 * time.Second)
+    defer cancel()
+    select {
+    case <-ctx.Done():
+        log.Println("shutdown timeout has expired")
     }
-
-    if err := conn.Close(); err != nil {
-        log.Fatal("failed to close connection:", err)
-    }
+    log.Println("consumer service exiting")
 }
